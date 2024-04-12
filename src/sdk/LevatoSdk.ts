96b3aae1505707314727a881a612ee70976ba432
k.zlatarov <k.zlatarov@dcvx.io>
2024-04-12 13:10:07 +0300

    Add max leverage ratio query

--- src/sdk/LevatoSdk.ts
+++ src/sdk/LevatoSdk.ts
@@ -1,4 +1,4 @@
-import { Signer } from 'ethers';
+import { Signer, formatEther } from 'ethers';
 import {
   FlashloanRouter,
   FlashloanRouter__factory,
@@ -53,7 +53,55 @@ export default class LevatoSDK {
   }
 
   // Getters
-  get signer(): Signer {
+  get getSigner(): Signer {
     return this.#signer;
+  }
+
+  // Public get methods
+
+  /**
+   * Get borrow rates for underlying assets
+   * @param assetsUnderlying
+   * @returns A map with addresses as keys and percentages as values
+   */
+  async getAssetsBorrowRates(
+    assetsUnderlying: string[]
+  ): Promise<Map<string, string> | undefined> {
+    const rates = await this.#lensContract.getAssetsBorrowRates.staticCall(
+      assetsUnderlying
+    );
+    const ratesMap: Map<string, string> = new Map();
+
+    if (rates && rates.length) {
+      for (let i = 0; i < rates.length; i++) {
+        ratesMap.set(
+          assetsUnderlying[i],
+          `${Number(formatEther(rates[i] * 100n)).toFixed(3)}%`
+        );
+      }
+    }
+
+    return ratesMap;
+  }
+
+  /**
+   * Get the max leverage ratio
+   * for a given collateral
+   * @param { string } collateralUnderlying
+   * @param { string } collateralAmount
+   * @param { string } borrowAssetUnderlying
+   */
+  async getMaxLeverageRatio(
+    collateralUnderlying: string,
+    collateralAmount: string,
+    borrowAssetUnderlying: string
+  ): Promise<bigint> {
+    const result = await this.#lensContract.getMaxLeverageRatio.staticCall(
+      collateralUnderlying,
+      collateralAmount,
+      borrowAssetUnderlying
+    );
+
+    return result;
   }
 }
