8863543d6a4b8d4645503e14309034f431aeabbd
k.zlatarov <k.zlatarov@dcvx.io>
2024-04-23 10:50:31 +0300

    Bump version and fix typechain export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,4 @@
 import LevatoSDK from './sdk/LevatoSdk';
+import * as LevatoTypechain from './typechain';
+export declare const typechain: typeof LevatoTypechain;
 export default LevatoSDK;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import LevatoSDK from './sdk/LevatoSdk';
+import * as LevatoTypechain from './typechain';
+
+export const typechain = LevatoTypechain;
 
 export default LevatoSDK;
