00435a27d56a5cdb3421c1c594855a72123f2c88
k.zlatarov <k.zlatarov@dcvx.io>
2024-06-05 15:59:40 +0300

    Add rolled up version

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,10 @@
 import merge from 'deepmerge';
 import { createBasicConfig } from '@open-wc/building-rollup';
 import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
 const baseConfig = createBasicConfig();
 
 export default merge(baseConfig, [
@@ -8,24 +12,34 @@ export default merge(baseConfig, [
     input: './src/index.ts',
     output: {
       dir: './dist',
-      format: 'esm'
+      format: 'commonjs'
     },
-    plugins: [typescript()]
-  },
-  {
-    input: './src/sdk/LevatoSdk.ts',
-    output: {
-      dir: './dist/sdk',
-      format: 'esm'
-    },
-    plugins: [typescript({ declaration: false, outDir: './dist/sdk' })]
-  },
-  {
-    input: './src/.graphclient/index.ts',
-    output: {
-      dir: './dist/.graphclient',
-      format: 'esm'
-    },
-    plugins: [typescript({ declaration: false, outDir: './dist/.graphclient' })]
+    plugins: [nodeResolve(), json(), commonjs(), typescript()]
   }
+  // {
+  //   input: './src/sdk/LevatoSdk.ts',
+  //   output: {
+  //     dir: './dist/sdk',
+  //     format: 'commonjs'
+  //   },
+  //   plugins: [
+  //     nodeResolve(),
+  //     json(),
+  //     commonjs(),
+  //     typescript({ declaration: false, outDir: './dist/sdk' })
+  //   ]
+  // },
+  // {
+  //   input: './src/.graphclient/index.ts',
+  //   output: {
+  //     dir: './dist/.graphclient',
+  //     format: 'commonjs'
+  //   },
+  //   plugins: [
+  //     nodeResolve(),
+  //     json(),
+  //     commonjs(),
+  //     typescript({ declaration: false, outDir: './dist/.graphclient' })
+  //   ]
+  // }
 ]);
