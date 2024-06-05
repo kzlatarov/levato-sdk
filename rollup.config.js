5941b12dfb349ff6e4c59a6aee1402a1fd5b9344
k.zlatarov <k.zlatarov@dcvx.io>
2024-06-05 14:48:04 +0300

    Prep deployment

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 import merge from 'deepmerge';
 import { createBasicConfig } from '@open-wc/building-rollup';
-
 const baseConfig = createBasicConfig();
 
 export default merge(baseConfig, {
   input: './out-ts/index.js',
   output: {
-    dir: 'dist'
+    dir: 'dist',
+    format: 'cjs'
   }
 });
